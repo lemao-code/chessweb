4ca5077486b75e51ee491f6855fd2aa72cc7a452
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 17:07:19 -0300

    ssl and objects

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,11 @@ module.exports = {
       database: "d85q2v8kvodqoq",
       user: "vaynfjykynglcb",
       password: "e2c65e6177100d5c73a9c57a06575dd934650acbeee3cfcef73c65f02a235ea4",
+      ssl: true,
+  dialect: 'postgres',
+  dialectOptions: {
+    "ssl": {"require":true }
+  }
     },
     migrations: {
       tableName: 'knex_migrations',
