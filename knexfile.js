ab53083d547ee17615ef15512e572a99e8615418
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 17:04:27 -0300

    host

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
+      host: "ec2-52-6-75-198.compute-1.amazonaws.com",
       database: "d85q2v8kvodqoq",
       user: "vaynfjykynglcb",
       password: "e2c65e6177100d5c73a9c57a06575dd934650acbeee3cfcef73c65f02a235ea4",
