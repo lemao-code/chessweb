ddc73dc3a6e052031ebfec45cf1d8c579c2c7ed1
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 09:11:40 -0300

    knex config

--- src/config/db.js
+++ src/config/db.js
@@ -1,3 +1,3 @@
 const knexfile = require('../../knexfile');
-const knex = require('knex')(knexfile[process.env.DB_ENVIROMENT || 'development'])
+const knex = require('knex')(knexfile[process.env.DB_ENVIRONMENT || 'development'])
 module.exports = knex;
