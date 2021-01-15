bacc30827ed7fa22e99b05872c03c96ae508b812
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 10:20:54 -0300

    ssl mode true

--- knexfile.js
+++ knexfile.js
@@ -11,9 +11,8 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    },
+    ssl: true,
+    sslmode: true,
     pool: {
       min: 2,
       max: 10
