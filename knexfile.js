5457aa1b27eddcfaad7754b03c947b848d290b26
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 17:09:08 -0300

    rejectUnauthorized

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,9 @@ module.exports = {
       database: "d85q2v8kvodqoq",
       user: "vaynfjykynglcb",
       password: "e2c65e6177100d5c73a9c57a06575dd934650acbeee3cfcef73c65f02a235ea4",
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false
+      },
   dialect: 'postgres',
   dialectOptions: {
     "ssl": {"require":true }
