5bcb3a3d9d8a9107d7c353df1007ff5b0f42d3ee
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 10:53:33 -0300

    require pg

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,15 @@
+const {Client } = require('pg')
+
+const client =  new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 module.exports = {
 
   development: {
-    client: 'pg',
+    client: client,
     connection: {
       database: 'chess',
       user: 'postgres',
