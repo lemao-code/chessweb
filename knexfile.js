8534f77af19e3a1caa2627b67c17a1dc61e4e633
lemao-code <th.ferrari@outlook.com.br>
2021-01-15 09:49:07 -0300

    ssl

--- knexfile.js
+++ knexfile.js
@@ -9,15 +9,17 @@ module.exports = {
     }
   },
   production: {
-    client: "pg",
+    client: 'pg',
     connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: './migrations'
     },
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: true
   },
   
     migrations: {
